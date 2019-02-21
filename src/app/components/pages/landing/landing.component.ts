3d78b30fd208da66baf38c2200e1215257c3bf53
Dominic <domleggio@gmail.com>
2019-02-21 11:04:39 -0800

    updated cards on a few pages

--- src/app/components/pages/landing/landing.component.ts
+++ src/app/components/pages/landing/landing.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { LeagueService } from 'src/app/services/entities/league.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { FooterService } from 'src/app/services/entities/footer.service';
+import { NavbarService } from 'src/app/services/entities/navbar.service';
 
 @Component({
   selector: 'app-landing',
@@ -14,11 +15,16 @@ export class LandingComponent implements OnInit {
   public league: any = [];
   public sport: any;
 
-  constructor(private leagueService: LeagueService, private auth: AuthService, private footer: FooterService) { }
+  constructor(
+    private leagueService: LeagueService, 
+    private auth: AuthService, 
+    private footer: FooterService,
+    private navbar: NavbarService) { }
 
   ngOnInit() {
 
     this.footer.show();
+    this.navbar.show();
     
     this.leagueService.getSports().subscribe(result => {
       console.log(result);
